cfe930ec26681bb050675ebe28c8175e5121a861
Rustam Alashrafov <rustam.alashrafov@gmail.com>
2016-02-07 23:10:45 +0100

    Added Rocket Reverse on Click functionality
    Now rocket will move to the opposite direction when its clicked

---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketController : MonoBehaviour {
+	private Rigidbody m_Rigidbody;
+	public float m_Acceleration = 5;
+	public Vector3 m_InitialVelocity;
+	// Use this for initialization
+	void Start () {
+		m_Rigidbody = GetComponent<Rigidbody> ();
+		m_Rigidbody.velocity = m_InitialVelocity;;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnMouseDown () {
+		ReverseDirection ();
+	}
+	void FixedUpdate() {
+		Move ();
+	}
+
+	private void ReverseDirection() {
+		m_Rigidbody.transform.forward = -m_Rigidbody.transform.forward;
+	}
+	private void Move() {
+		m_Rigidbody.AddForce (transform.forward*m_Acceleration, ForceMode.Impulse);
+	}
+}
