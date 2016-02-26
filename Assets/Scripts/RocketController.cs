6a3624557c1f95534783fd0da1d91734871bda4b
Rustam Alashrafov <rustam.alashrafov@gmail.com>
2016-02-26 13:22:00 +0100

    Separated Time Controller.
    Made time controller Class extendable. Now we can affect multiple
    objects at once.

--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class RocketController : MonoBehaviour {
 	private Rigidbody m_Rigidbody;
+	public TimeController m_TimeController;
 	//private string m_TimeStopAxisName = "TimeStop";
 	//private float m_TimeStopAxisValue;
 	public float m_Acceleration = 5f;
 	public float m_InitialSpeed = 30f; //m/s //TODO: How to adjust variable to correspond to player movement
-	public float m_TimeScale = 0.00000001f;
 
-	bool m_TimeIsScaled = false;
+	private float m_PrevTimeScale; //store the value of time scale of the previous frame. Required to check if the scale has changed
 
 	Vector3 m_NotScaledVelocity;
 	// Use this for initialization
@@ -18,19 +18,12 @@ public class RocketController : MonoBehaviour {
 		m_Rigidbody = GetComponent<Rigidbody> ();
 		m_Rigidbody.velocity = transform.forward * m_InitialSpeed;
 		m_NotScaledVelocity = m_Rigidbody.velocity ;
+		m_PrevTimeScale = Time.timeScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		//m_TimeStopAxisValue = Input.GetAxis (m_TimeStopAxisName);
-		//if (m_TimeStopAxisValue != 0f) {
-			//ToggleStopTime ();
-		//}
-
-		if(Input.GetKeyDown("left shift")){ 
-			ToggleTimeScale ();
-		}
+		UpdateOnTimeScale();
 	}
 	void OnMouseDown () {
 		ReverseDirection ();
@@ -43,22 +36,29 @@ public class RocketController : MonoBehaviour {
 		m_Rigidbody.transform.forward = -m_Rigidbody.transform.forward;
 	}
 	private void Move() {
-		if (m_TimeIsScaled) {
-			m_Rigidbody.velocity = m_Rigidbody.velocity * m_TimeScale; //TODO: thi works but we don't know why!!! but works for now. We should NOT scale velocity every update
-		}
+
+		//TODO: SEE_LEDIO: Gor rid of this. it was wrong. It worked fine with the one time scale. the problem is when you change the time scale, velocity is always very small.
+		//Now I change velocity once in UpdateOnTimeScale();
+//		if (m_TimeController.IsTimeScaled()) {
+//			//m_Rigidbody.velocity = m_Rigidbody.velocity * Time.timeScale; //TODO: thi works but we don't know why!!! but works for now. We should NOT scale velocity every update
+//		}
 			
 		m_Rigidbody.AddForce (transform.forward*m_Acceleration*Time.deltaTime, ForceMode.Impulse);
 	}
 
-	private void ToggleTimeScale() {
-		if (Time.timeScale == 1f) {
-			Time.timeScale = m_TimeScale;
-			m_TimeIsScaled = true;
-			m_NotScaledVelocity = m_Rigidbody.velocity;
-		} else {
-			Time.timeScale = 1f;
-			m_TimeIsScaled = false;
-			m_Rigidbody.velocity = m_NotScaledVelocity;
+	
+	//TODO: SEE_LEDIO: This function will handle everthing that has to be done with object after time scale has changed.
+	// Will need similar funciton for each class we want to be affected by time scale
+	private void UpdateOnTimeScale(){
+		if (m_PrevTimeScale != Time.timeScale) { 
+			if (m_TimeController.IsTimeScaled ()) {
+				m_NotScaledVelocity = m_Rigidbody.velocity;
+				m_Rigidbody.velocity = m_Rigidbody.velocity * Time.timeScale; // Here is the proper place to update velocity. however we get now jiggling
+			} else {
+				m_Rigidbody.velocity = m_NotScaledVelocity;
+			}
 		}
+		m_PrevTimeScale = Time.timeScale; //update m_PrevTimeScale to the current time scale after we done cheking if it changed.
+
 	}
 }
---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeController : MonoBehaviour {
+
+	public float m_TimeScale = 0.00000001f;
+	private bool m_TimeIsScaled = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//m_TimeStopAxisValue = Input.GetAxis (m_TimeStopAxisName);
+		//if (m_TimeStopAxisValue != 0f) {
+		//ToggleStopTime ();
+		//}
+		if(Input.GetKeyDown("left shift")){ 
+			ToggleTimeScale ();
+		}
+	}
+
+
+
+	private void ToggleTimeScale() {
+		if (Time.timeScale == 1f) {
+			Time.timeScale = m_TimeScale;
+			m_TimeIsScaled = true;
+		} else {
+			Time.timeScale = 1f;
+			m_TimeIsScaled = false;
+		}
+	}
+
+	//getter for m_TimeIsScaled. Protecting the variable from changing from outside the class.
+	public bool IsTimeScaled() {
+		return m_TimeIsScaled;
+	}
+}
